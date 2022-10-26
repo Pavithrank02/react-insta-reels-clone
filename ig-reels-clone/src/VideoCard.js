b0e993244e7b42f2e6002592d47e190a1e5863e5
Pavithran <pavithrank02@gmail.com>
2022-10-26 21:01:32 +0530

    play and pause feature

--- ig-reels-clone/src/VideoCard.js
+++ ig-reels-clone/src/VideoCard.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './VideoCard.css';
 
 function VideoCard() {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const videoRef = useRef(null); 
+  const onVideoPress = () => {
+    if(isVideoPlaying) {
+      videoRef.current.pause();
+      setIsVideoPlaying(false)
+    }else {
+      videoRef.current.play()
+      setIsVideoPlaying(true);
+    }
+
+  }
   return (
     <div className='videoCard'>
         <video 
-        className='video__player'
+        ref={videoRef}
+        onClick={onVideoPress}
+        className='videoCard__player'
         src='https://player.vimeo.com/external/420237319.sd.mp4?s=c25da38b732e29a30ed7f974b2d0199f5f7e072e&profile_id=165&oauth2_token_id=57447761'
+        alt='IG reels video'
         loop
         />
     </div>
