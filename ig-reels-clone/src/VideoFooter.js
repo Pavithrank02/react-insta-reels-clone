3343ee0d81350050b28f6ad961ec7fe1b4b48203
Pavithran <pavithrank02@gmail.com>
2022-11-04 08:55:26 +0530

    placed okay

--- ig-reels-clone/src/VideoFooter.js
+++ ig-reels-clone/src/VideoFooter.js
@@ -2,6 +2,10 @@ import { Avatar, Button } from "@mui/material";
 import React from "react";
 import "./VideoFooter.css";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SendIcon from "@mui/icons-material/Send";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import CommentIcon from "@mui/icons-material/Comment";
 
 function VideoFooter({ likes, shares, channel, avatarSrc, song }) {
   return (
@@ -13,9 +17,27 @@ function VideoFooter({ likes, shares, channel, avatarSrc, song }) {
         </h3>
       </div>
       <div className="videoFooter__ticker">
-        <MusicNoteIcon className="videoFooter__icon" />\{" "}
+        <MusicNoteIcon className="videoFooter__icon" />
+        <h1> {song}</h1>
       </div>
-      <div className="videoFooter__ticker"></div>
+      <div className="videoFooter__actions">
+        <div className="videoFooter__actionsLeft">
+          <FavoriteIcon />
+          <SendIcon />
+          <MoreHorizIcon />
+          <CommentIcon />
+        </div>
+        <div className="videoFooter__actionsRight">
+          <div className="videoFooter__stat">
+            <FavoriteIcon fontSize="small" />
+            <p>{likes}</p>
+          </div>
+          <div className="videoFooter__stat">
+            <CommentIcon fontSize="small" />
+            <p>{shares}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- ig-reels-clone/src/VideoHeader.js
+++ ig-reels-clone/src/VideoHeader.js
@@ -2,17 +2,6 @@ import React from "react";
 import "./VideoHeader.css";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
-
-
-
-
-
-
-
-
-
-
-
 function VideoHeader() {
   return (
     <div className="videoHeader">
